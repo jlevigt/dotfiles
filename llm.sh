9baf8d90ffad174f2e6decf4018efe7daf00344a
jlevigt <jlevi.gt@gmail.com>
2026-01-04 15:55:10 -0300

    .

---
+++ llm.sh
@@ -0,0 +1,10 @@
+curl -fsSL https://ollama.com/install.sh | sh
+
+# Download LM Studio Linux .AppImage
+# https://lmstudio.ai/download
+
+# Make it executable
+chmod +x LM-Studio*.AppImage
+
+# Run it
+./LM-Studio*.AppImage
